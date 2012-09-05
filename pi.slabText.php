309356a7fe35fa66ec4bfc8414bf4eb3f6cb6950
Brian McAllister <frequency.decoder@gmail.com>
2012-09-05 19:51:14 +0200

    Rewrite

--- pi.slabText.php
+++ pi.slabText.php
@@ -1,28 +1,14 @@
 <?php
 class Plugin_slabtext extends Plugin {
     public function index() {
-        $title  = $this->fetch_param('title', null);
-        $class  = $this->fetch_param('class', "");
-        $delim  = $this->fetch_param('delimiter', "|");
-        
-        if($title === null) {
-            return false;
+        $title  = $this->fetch_param('title', "");
+        $delim  = $this->fetch_param('delim', "|");
+
+        if(strpos($title, $delim) === false) {
+            return $title;
         };
-        
-        if(strpos($delim, $title) === false) {
-            return array(
-                "slabtexted"    => $title,
-                "class"         => ""
-                );
-        };
-        
-        $parts = '<span class="slabtext">' . implode('</span><span class="slabtext">', explode($delim, $title)) . '</span>';
-        $class = ' class="' . trim('slabtext ' . $class) . '"';
-        
-        return array(
-            "slabtexted"    => $parts,
-            "class"         => $class
-        );
-    };
-}
+
+        return '<span class="slabtext">' . implode('</span><span class="slabtext">', explode($delim, $title)) . '</span>';
+    }
+};
 ?>
